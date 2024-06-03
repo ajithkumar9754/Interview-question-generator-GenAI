4392ee9fe91adcd210f446a135094ba4c9f6a861
ajithkumar9754 <ajithkumar9754@gmail.com>
2024-06-03 13:56:32 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project - Interview question Generator",
+    version= '0.0.0',
+    author="Ajith Kumar",
+    author_email="ajithkumar9754@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
